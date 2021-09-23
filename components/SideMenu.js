e2882a99d6ba2dd38e3b66d0f8724300e165b841
WalterOkumu <okumu.oriaro@gmail.com>
2021-09-23 14:40:17 +0300

    initial commit

---
+++ components/SideMenu.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+const SideMenu = (props) => {
+
+    return (
+        <div>
+            <button onClick = {props.clickHandler}>Click Me</button>
+            <h1 className="my-4">{props.appName}</h1>
+            <div className="list-group">
+            <a href="#" className="list-group-item">Category 1</a>
+            <a href="#" className="list-group-item">Category 2</a>
+            <a href="#" className="list-group-item">Category 3</a>
+            </div>
+
+            <div>
+                <h1>Number: {props.count}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default SideMenu
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// functional component
+const About = () => {
+    return (
+        <h1>About Page</h1>
+    )
+}
+
+export default About
